d3daeec97fc3655b07b92fb1bb8e617cf78b755f
smvenkateshvignesh <vvicky5270@gmail.com>
2019-06-08 10:29:24 +0530

    Added the SplashScreen

---
+++ app/src/main/java/com/example/videoplayer/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.videoplayer.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import com.example.videoplayer.R
+import com.example.videoplayer.ui.videoList.VideoListActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+
+class SplashScreen : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 2500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val animZoomIn = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out)
+        imgAppLogo.startAnimation(animZoomIn)
+        runHandler()
+    }
+    private fun runHandler() {
+        Handler().postDelayed({
+            navigateToScreen()
+        }, SPLASH_TIME_OUT)
+    }
+
+    private fun navigateToScreen() {
+        startActivity(Intent(this, VideoListActivity::class.java))
+        finish()
+    }
+
+}
